385a45b6b1025d094afca2e8e6eb47bd0acf65a0
antonio.pocas <antonio.pocas@farfetch.com>
2017-05-10 18:01:48 +0100

    Did more things

--- LSBASI/LSBASI/Program.cs
+++ LSBASI/LSBASI/Program.cs
@@ -6,23 +6,27 @@ using System.Threading.Tasks;
 
 namespace LSBASI
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var x = Console.ReadLine();
             var interpreter = new Interpreter(x);
-            var y = interpreter.DoTheThings();
+            var y = interpreter.Evaluate();
             Console.WriteLine(y);
             Console.ReadLine();
         }
     }
 
-    class Interpreter
+    internal class Interpreter
     {
         public Token CurrentToken { get; set; }
         public int Position { get; set; }
         public string Text { get; set; }
+
+        public string CurrentChar => Position < Text.Length
+            ? Text[Position].ToString()
+            : null;
 
         public Interpreter(string input)
         {
@@ -33,52 +37,70 @@ namespace LSBASI
 
         public Token GetNextToken()
         {
-            if(Position > Text.Length - 1)
+            if (Position > Text.Length - 1)
             {
                 return Token.CreateEOFToken();
             }
 
-            while(Text[Position] == ' ')
+            while (CurrentChar == " ")
             {
                 Position++;
             }
 
-            var currentChar = GetCurrentCharAsString();
-            var sb = new StringBuilder();
+            if (CurrentChar == null)
+            {
+                return Token.CreateEOFToken();
+            }
 
-            bool isInt = false;
             int tempValue;
-            while(int.TryParse(currentChar, out tempValue))
+            if (int.TryParse(CurrentChar, out tempValue))
             {
-                isInt = true;
-                sb.Append(currentChar);
-                Position++;
-                if (Position < Text.Length)
-                {
-                    currentChar = GetCurrentCharAsString();
-                }
-                else
-                {
-                    break;
-                }
-            }
-            if(isInt)
-            {
-                return new Token(TokenType.Integer, sb.ToString());
+                return new Token(TokenType.Integer, TakeInteger());
             }
 
-            if(currentChar == "+")
+            if (CurrentChar == "+")
             {
                 Position++;
-                return new Token(TokenType.Plus, currentChar);
+                return new Token(TokenType.Add, CurrentChar);
+            }
+
+            if (CurrentChar == "-")
+            {
+                Position++;
+                return new Token(TokenType.Subtract, CurrentChar);
+            }
+
+            if (CurrentChar == "*")
+            {
+                Position++;
+                return new Token(TokenType.Multiply, CurrentChar);
+            }
+
+            if (CurrentChar == "/")
+            {
+                Position++;
+                return new Token(TokenType.Divide, CurrentChar);
             }
 
             throw new InvalidOperationException("Error parsing input");
         }
 
+        private string TakeInteger()
+        {
+            var sb = new StringBuilder();
+            int tempValue;
+            while (int.TryParse(CurrentChar, out tempValue))
+            {
+                sb.Append(CurrentChar);
+                Position++;
+            }
+
+            return sb.ToString();
+        }
+
         public void Eat(TokenType type)
         {
-            if(CurrentToken.Type == type)
+            if (CurrentToken.Type == type)
             {
                 CurrentToken = GetNextToken();
             }
@@ -88,20 +110,59 @@ namespace LSBASI
             }
         }
 
-        public int DoTheThings()
+        public int Evaluate()
         {
             CurrentToken = GetNextToken();
+            int tempResult = 0;
+            bool isFirst = true;
+            while (CurrentToken.Type != TokenType.EOF)
+            {
+                Token left;
+                if (isFirst)
+                {
+                    left = CurrentToken;
+                    Eat(TokenType.Integer);
+                    isFirst = false;
+                }
+                else
+                {
+                    left = new Token(TokenType.Integer, tempResult.ToString());
+                }
 
-            var left = CurrentToken;
-            Eat(TokenType.Integer);
+                var operation = CurrentToken;
+                Func<int, int, int> operationFunction;
+                switch (operation.Type)
+                {
+                    case TokenType.Add:
+                        Eat(TokenType.Add);
+                        operationFunction = (x, y) => x + y;
+                        break;
 
-            var operation = CurrentToken;
-            Eat(TokenType.Plus);
+                    case TokenType.Subtract:
+                        Eat(TokenType.Subtract);
+                        operationFunction = (x, y) => x - y;
+                        break;
 
-            var right = CurrentToken;
-            Eat(TokenType.Integer);
+                    case TokenType.Multiply:
+                        Eat(TokenType.Multiply);
+                        operationFunction = (x, y) => x * y;
+                        break;
 
-            return Int32.Parse(left.Value) + Int32.Parse(right.Value);
+                    case TokenType.Divide:
+                        Eat(TokenType.Divide);
+                        operationFunction = (x, y) => x / y;
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Error parsing input");
+                }
+
+                var right = CurrentToken;
+                Eat(TokenType.Integer);
+                tempResult = operationFunction(Int32.Parse(left.Value), Int32.Parse(right.Value));
+            }
+
+            return tempResult;
         }
 
         private string GetCurrentCharAsString()
@@ -110,7 +171,7 @@ namespace LSBASI
         }
     }
 
-    class Token
+    internal class Token
     {
         public TokenType Type { get; set; }
         public string Value { get; set; }
@@ -132,11 +193,13 @@ namespace LSBASI
         }
     }
 
-    enum TokenType
+    internal enum TokenType
     {
         Integer,
-        Plus,
-        Minus,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
         EOF
     }
 }
